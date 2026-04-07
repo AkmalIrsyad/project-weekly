d420322fc9b7369251a2ec7cb67ebb4bdde50cf6
akmal irsyad <akmalirsyad137@gmail.com>
2026-04-07 12:45:43 +0700

    feat: update ChannelModel add lastMessage, time, unreadCount

--- lib/data/model/channel_model.dart
+++ lib/data/model/channel_model.dart
@@ -3,11 +3,17 @@ class ChannelModel {
   final String seed;
   final String followers;
   final bool verified;
+  final String? lastMessage;
+  final String? time;
+  final int unreadCount;
 
   const ChannelModel({
     required this.name,
     required this.seed,
     required this.followers,
     required this.verified,
+    this.lastMessage,
+    this.time,
+    this.unreadCount = 0,
   });
 }
